cd570235f88787dee5c998478f7a0ff25e72b5aa
Michael Jones <yardquit@pm.me>
2026-07-28 15:41:46 +0200

    Bumping version to 1.2.0

--- donkey.el
+++ donkey.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Michael Jones
 ;; Assisted-by: Lumo 2.0 Max, Claude [Claude Code]
 ;; URL: https://github.com/yardquit/donkey
-;; Version: 1.1.1
+;; Version: 1.2.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience
 ;; Homepage: https://github.com/yardquit/donkey
